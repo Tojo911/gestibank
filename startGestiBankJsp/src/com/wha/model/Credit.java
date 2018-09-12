145a0c71b039ee9dfb95d2136f139e8d31398dd4
marie <ma.assogbavi@gmail.com>
2018-09-12 15:02:37 +0200

    Marie 15h02

---
+++ startGestiBankJsp/src/com/wha/model/Credit.java
@@ -0,0 +1,14 @@
+package com.wha.model;
+
+import javax.persistence.DiscriminatorValue;
+
+@DiscriminatorValue("OPCRE")
+public class Credit extends Operation {
+
+	public Credit() {
+		// TODO Auto-generated constructor stub
+		
+		// tester envoi
+	}
+
+}
---
+++ startGestiBankJsp/src/com/wha/model/Debit.java
@@ -0,0 +1,12 @@
+package com.wha.model;
+import javax.persistence.DiscriminatorValue;
+
+
+@DiscriminatorValue("OPDEB")
+public class Debit extends  Operation{
+
+	public Debit() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
